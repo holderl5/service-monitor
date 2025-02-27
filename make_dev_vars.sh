d5d367590cc6bb2e031b2e2397d572c6fe6291ba
JHolder <holder@l5sg.com>
2025-02-27 14:57:56 -0600

    dump wrangler.toml additions to console when making .dev.vars

--- make_dev_vars.sh
+++ make_dev_vars.sh
@@ -5,3 +5,10 @@ con=$(cat config.json | jq -r '@json' | sed -e 's%\\%%g')
 
 echo "RESOURCES = $res" > .dev.vars
 echo "CONFIG = $con" >> .dev.vars
+
+# echo the data for wrangler.toml for now:
+echo "Replace vars in wrangler.toml with the following:"
+echo
+echo "[vars]"
+echo "RESOURCES = $(cat resources.json | jq '@json')"
+echo "CONFIG=$(cat config.json | jq '@json')"
